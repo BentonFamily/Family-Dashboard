2963a43691f46e375c4b852269665fb97062f5fa
BentonFamily <benton.family19@gmail.com>
2025-06-17 21:03:23 +0100

    Temp commit of config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+window.config = {
+  wifiName: "BenBen",
+  wifiPassword: "CarHenRobMea!123",
+  calendarEmail: "benton.family19@gmail.com",
+  sheetsApiKey: "AIzaSyAzLBA3Hirfns2CEy2rjm4UpaatNJ45P8s"
+};
