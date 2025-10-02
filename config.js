d46c112583c1f972326303f8a5828641307d85a9
BentonFamily <benton.family19@gmail.com>
2025-10-02 22:58:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,6 @@ window.config = {
   wifiPassword: "CarHenRobMea!123",
   calendarEmail: "benton.family19@gmail.com",
   sheetsApiKey: "AIzaSyAzLBA3Hirfns2CEy2rjm4UpaatNJ45P8s"
+  choreSubmitMode: 'script',
+  choreSubmitUrl: 'https://script.google.com/macros/s/AKfycbzMaTIIfPvvbe7suLhr_m3x0mhdJ-exEe5OSliVtB1aKSb9DBBfhft6a069sHvTko1m/exec'
 };
