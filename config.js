bb01e95cb461713b6bac9019752399eb40fed8d6
BentonFamily <benton.family19@gmail.com>
2025-10-23 22:39:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ window.config = {
 
   // Your Apps Script endpoints
   summaryUrl: "https://script.google.com/macros/s/AKfycbwBIGo4EnzoGMU7y6jG7VITAiH6o0aVnQr9rlO9IQ6GdXlwz9eOyw0x5nfP1bEkEfLNzg/exec", // GET
-  choreSubmitUrl: "https://script.google.com/macros/s/AKfycbzMaTIIfPvvbe7suLhr_m3x0mhdJ-exEe5OSliVtB1aKSb9DBBfhft6a069sHvTko1m/exec", // POST
+  choreSubmitUrl: "https://script.google.com/macros/s/AKfycbzkkjsvihexCz6KixbW0MPjEx1KX8ZnUS-RmEPkbtfFbIKLoF7U6wYMxOGN6ujZT4SFWg/exec", // POST
 
   // Keep true if you want it unlocked by default during your testing
   defaultUnlocked: false
