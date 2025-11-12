74acae7d88e317d1087d79cd8df3fe11d9c0e72b
BentonFamily <benton.family19@gmail.com>
2025-11-12 01:39:47 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ window.config = {
   calendarEmail: "benton.family19@gmail.com",
 
   // Your Apps Script endpoints
-  summaryUrl: "https://script.google.com/macros/s/AKfycbwBIGo4EnzoGMU7y6jG7VITAiH6o0aVnQr9rlO9IQ6GdXlwz9eOyw0x5nfP1bEkEfLNzg/exec", // GET
+  summaryUrl: "https://script.google.com/macros/s/AKfycbzkkjsvihexCz6KixbW0MPjEx1KX8ZnUS-RmEPkbtfFbIKLoF7U6wYMxOGN6ujZT4SFWg/exec", // GET
   choreSubmitUrl: "https://script.google.com/macros/s/AKfycbzkkjsvihexCz6KixbW0MPjEx1KX8ZnUS-RmEPkbtfFbIKLoF7U6wYMxOGN6ujZT4SFWg/exec", // POST
 };
