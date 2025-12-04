d941678dcb7afaed77f0f53f789250f55ee5dbef
BentonFamily <benton.family19@gmail.com>
2025-12-04 22:35:53 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ window.config = {
   // Your Apps Script endpoints
   summaryUrl: "https://script.google.com/macros/s/AKfycbzkkjsvihexCz6KixbW0MPjEx1KX8ZnUS-RmEPkbtfFbIKLoF7U6wYMxOGN6ujZT4SFWg/exec", // GET
   choreSubmitUrl: "https://script.google.com/macros/s/AKfycbzkkjsvihexCz6KixbW0MPjEx1KX8ZnUS-RmEPkbtfFbIKLoF7U6wYMxOGN6ujZT4SFWg/exec", // POST
-  spellingsUrl: 'https://script.google.com/macros/s/AKfycby2oUxYJt3ZdmiH08IJCwEWabQ70P541ZHRz5l8mDmJkanH6kZKiH2mwzVPaf2aBD3dMg/exec'
+  spellingsUrl: 'https://script.google.com/macros/s/AKfycbwlFg80S5ZqJ50fbeVw7TKneaM0QkLroM6_QCholV4ysClRDDG3pydrlxqGluiCBydcFw/exec'
 };
